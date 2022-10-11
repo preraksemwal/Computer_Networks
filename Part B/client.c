7c6e26db5bb76eea4eabd717b2fc63dddacf3b7d
preraksemwal <preraksemwal@gmail.com>
2022-10-11 21:08:03 +0530

    done

--- Part B/client.c
+++ Part B/client.c
@@ -10,18 +10,22 @@
 
 #define PORT 4444
 
-void* routine(int i) {
-    printf("%d", i);
-    int socket_fd, ret, n;
+int check(int ret, const char* message) {
+	if (ret < 0) {
+		perror(message);
+		exit(1);
+	}
+	return ret;
+}
+
+void* handle_connection(void* ptr_i) {
+    int i = *((int*)ptr_i);
+    free(ptr_i);
+    int socket_fd;
 	struct sockaddr_in serverAddr;
 	char buffer[1024];
 
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if(socket_fd < 0){
-		printf("[-]Error in connection.\n");
-		exit(1);
-	}
-	printf("[+]Client Socket is created.\n");
+	check(socket_fd = socket(AF_INET, SOCK_STREAM, 0), "Client Socket Creation Failed.\n");
 
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
@@ -29,30 +33,23 @@ void* routine(int i) {
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     // bcopy((char*) server->h_addr, (char*) &server_addr.sin_addr.s_addr, server->h_length);
 
-	ret = connect(socket_fd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if(ret < 0){
-		printf("[-]Error in connection.\n");
-		exit(1);
-	}
-	printf("[+]Connected to Server.\n");
+	check(connect(socket_fd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)), "Connect Failed.\n");
 
-    n = read(socket_fd, buffer, 255);
-    if (n < 0) {
-        perror("Read Failed");
-        exit(1);
-    }
+    check(read(socket_fd, buffer, 255), "Read Failed.\n");
+    
     unsigned long long int  factorial;
     write(socket_fd, &i, sizeof(int));
-    if (i > 20)
-        return;
     read(socket_fd, &factorial, sizeof(unsigned long long int ));
     printf("Factorial of %d = %llu\n", i, factorial);
+    return NULL;
 }
 
 int main(){
     for (int i = 1; i <= 10; ++i) {
         pthread_t t;
-        pthread_create(&t, NULL, &routine, i);
+        int* ptr_i = malloc(sizeof(int));
+		*ptr_i = i;
+        pthread_create(&t, NULL, handle_connection, ptr_i);
         pthread_join(t, NULL);
     }
 	return 0;
--- Part B/server.c
+++ Part B/server.c
@@ -9,70 +9,59 @@
 
 #define PORT 4444
 
+int check(int ret, const char* message) {
+	if (ret < 0) {
+		perror(message);
+		exit(1);
+	}
+	return ret;
+}
+
 int main(){
     remove("file.txt");
 
-	int socket_fd, ret, new_socket_fd;
-	struct sockaddr_in server_addr, newAddr;
+	int server_socket, ret, client_socket;
+	struct sockaddr_in server_addr, client_addr;
 	socklen_t addr_size;
 	char buffer[1024];
 	pid_t childpid;
 
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if(socket_fd < 0){
-		printf("[-]Error in connection.\n");
-		exit(1);
-	}
-	printf("[+]Server Socket is created.\n");
+	check(server_socket = socket(AF_INET, SOCK_STREAM, 0), "ServerSocket Creation Failed.\n");
 
 	memset(&server_addr, '\0', sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(PORT);
 	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	ret = bind(socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	if(ret < 0){
-		printf("[-]Error in binding.\n");
-		exit(1);
-	}
-	printf("[+]Bind to port %d\n", 4444);
+	check(bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)), "Binding Failed.\n");
 
-	if(listen(socket_fd, 10) == 0){
-		printf("[+]Listening....\n");
-	}else{
-		printf("[-]Error in binding.\n");
-	}
+	check(listen(server_socket, 10), "Listen Failed.\n");
 
     int t = 10;
 	while(t > 0){
         t = t - 1;
-		new_socket_fd = accept(socket_fd, (struct sockaddr*)&newAddr, &addr_size);
-		if(new_socket_fd < 0){
-			exit(1);
-		}
-		printf("Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+		check(client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size), "Accept Failed.\n");
+		
+		printf("Connection accepted from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
 		if((childpid = fork()) == 0){
-			close(socket_fd);
+			close(server_socket);
             FILE* file_ptr = fopen("file.txt", "a");
-            int number, n;
+            int number;
             unsigned long long int factorial = 1;
-            n = write(new_socket_fd, "Enter the number: ", strlen("Enter the number: "));
-            if (n < 0) {
-                perror("Write Failed.\n");
-                exit(1);
-            }
-            read(new_socket_fd, &number, sizeof(int));
+            check(write(client_socket, "Enter the number: ", strlen("Enter the number: ")), "Write Failed.\n");
+    
+            read(client_socket, &number, sizeof(int));
 
             for (int j = 2; j <= number; ++j)   factorial *= j;
             
             char address[INET_ADDRSTRLEN];
             inet_ntop( AF_INET, &server_addr.sin_addr, address, sizeof(address));    
             fprintf(file_ptr, "Address: %s, Port: %d, Factorial of %d: %llu\n", address, htons(server_addr.sin_port), number, factorial);
-            write(new_socket_fd, &factorial, sizeof(unsigned long long int ));
+            write(client_socket, &factorial, sizeof(unsigned long long int ));
 		}
 	}
 
-	close(new_socket_fd);
+	close(client_socket);
 	return 0;
 }
